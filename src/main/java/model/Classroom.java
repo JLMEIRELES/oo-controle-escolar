d78165fff20a6f8d1feb25688e81ae825070a5e7
juliaryoshida <juliayosh@gmail.com>
2023-01-30 20:38:13 -0300

    inclusão de classes

---
+++ src/main/java/model/Classroom.java
@@ -0,0 +1,33 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "classroom")
+
+public class Classroom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String classroom_code;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setClassroomCode(String classroom_code) {
+        this.classroom_code = classroom_code;
+    }
+    public String getClassroomCode() {
+        return classroom_code;
+    }
+
+    @ManyToOne
+    private Building building;
+}
