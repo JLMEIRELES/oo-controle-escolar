49c07a62146bd5ff7ddbde9446f107a05b81e890
luanduartee <luanmateuscd@gmail.com>
2023-02-06 19:09:31 -0300

    Team Interface

---
+++ src/main/java/gui/CreatTeamGUI.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.*;
+
+public class CreatTeamGUI {
+
+    private JPanel pagina;
+    private JLabel turmas;
+    private JTable tabelaTurma;
+    private JTextField InserirTurma;
+    private JLabel textAluno;
+    private JButton adicionarButton;
+    private JComboBox comboBox1;
+    private JLabel TextFiltro;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
