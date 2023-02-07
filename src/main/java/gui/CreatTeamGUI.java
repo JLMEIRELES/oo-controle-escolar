62bebe07f8f8f3338a2057863213b72f6fe524ed
luanduartee <luanmateuscd@gmail.com>
2023-02-07 20:16:19 -0300

    implementação Swing Turma

--- src/main/java/gui/CreatTeamGUI.java
+++ src/main/java/gui/CreatTeamGUI.java
@@ -12,6 +12,8 @@ public class CreatTeamGUI {
     private JButton adicionarButton;
     private JComboBox comboBox1;
     private JLabel TextFiltro;
+    private JSeparator separator1;
+    private JToolBar.Separator separadorCentral;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
